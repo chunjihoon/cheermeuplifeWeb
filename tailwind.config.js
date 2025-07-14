ffa1c2d9dcf047049452d0fccd12c234421a0919
Chris.chun.june <mint3081@naver.com>
2025-07-14 16:59:54 -0400

    20250714-3

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+      extend: {
+        // 아래처럼 커스텀 폰트 지정 가능
+        fontFamily: {
+          giants: ['Giants-Bold', 'sans-serif'],
+          sans: ['"Noto Sans KR"', 'Apple SD Gothic Neo', 'sans-serif'],
+          // 예시: Giants: ['Giants-Bold'], 등
+        },
+      },
+    },
+    plugins: [],
+};
